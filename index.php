5a4daf989cf53ca8cd0505004dd53c2861a0ae7f
Filipe Molina <filipemolina@live.com>
2017-03-28 12:46:26 -0300

    Ajuste

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'/../../Laravel/fw_trabalho/bootstrap/autoload.php';
+require __DIR__.'/../../fw_trabalho/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__.'/../../Laravel/fw_trabalho/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../../Laravel/fw_trabalho/bootstrap/app.php';
+$app = require_once __DIR__.'/../../fw_trabalho/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
